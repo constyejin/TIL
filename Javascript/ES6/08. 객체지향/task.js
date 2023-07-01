fd7db32806d116dc04ef74af46842a3641a13ea4
constyejin <lightup.jin@gmail.com>
2023-07-01 23:44:00 +0900

    Pick 3 identical objects at once using constructor syntax

---
+++ Javascript/ES6/08. 객체지향/task.js
@@ -0,0 +1,6 @@
+// 문제 1
+// constructor 문법을 사용해서 똑같은 object 3개 한번에 뽑기
+// 학생1.sayHi()로 사용하면  "안녕 나는 Kim이야" 라는 글자가 나오도록 sayHi()함수 constructor 안에 추가
+var 학생1 = { name : 'Kim', age : 20 }
+var 학생2 = { name : 'Park', age : 21 }
+var 학생3 = { name : 'Lee', age : 22 }
