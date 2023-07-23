3e292c5b850cf7dafca55f46c3c6fdc0c84b2750
constyejin <lightup.jin@gmail.com>
2023-07-23 22:26:09 +0900

    ♻️ 확장성 있는 코드로 변경 (add tabmenu4)

--- Javascript/Javascript Basic/15. 탭기능 (for 반복문)/js/script.js
+++ Javascript/Javascript Basic/15. 탭기능 (for 반복문)/js/script.js
@@ -29,17 +29,31 @@ let buttons = $('.tab-button');
 //   $('.tab-content').eq(2).addClass('show');
 // })
 
-// 좋은 관습 : 자주 쓰는 셀렉터 변수에 저장
 
-for (let i = 0; i < 3; i++) {
-  $('.tab-button').eq(i).on('click', function(){
-    $('.tab-button').removeClass('orange');
+// 반복문으로 작성
+// for (let i = 0; i < 3; i++) {
+//   $('.tab-button').eq(i).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-content').removeClass('show');
+
+//     $(this).addClass('orange');
+//     $('.tab-content').eq(i).addClass('show');
+//   })
+// }
+
+
+// (응용) 확장성 있는 코드로 변경
+// 좋은 코드의 조건
+// 1. 원하는 기능이 잘 구현 되었는지
+// 2. 확장성이 좋은지
+// 3. 나중에 관리가 용이한지
+// 4. 성능문제는 없는지
+for(let i = 0; i < buttons.length; i++) {
+  buttons.eq(i).on('click', function(){
+    buttons.removeClass('orange');
     $('.tab-content').removeClass('show');
 
     $(this).addClass('orange');
     $('.tab-content').eq(i).addClass('show');
   })
 }
-
-
-// 
