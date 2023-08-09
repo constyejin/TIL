d3a6731dccb247e98bfeb67bcd3a7c86028379fc
constyejin <lightup.jin@gmail.com>
2023-08-09 11:00:00 +0900

    Add comment explanation for problem 1

--- Javascript/ES6/08. 객체지향/task.js
+++ Javascript/ES6/08. 객체지향/task.js
@@ -1,16 +1,20 @@
 // 문제 1
 // constructor 문법을 사용해서 똑같은 object 3개 한번에 뽑기
 // 학생1.sayHi()로 사용하면  "안녕 나는 Kim이야" 라는 글자가 나오도록 sayHi()함수 constructor 안에 추가
+console.log(this);
 
 function Student(name, age){
-  this.name = name;
-  this.age = age;
+  // this : Student {}
+  // console.log(this)
+  this.a = name; // 매개변수 name으로 전달받은 값 this.name(key) = name(value)
+  this.b = age;  // 매개변수 age로 전달받은 값 this.age(key) = age(value)
   this.sayHi = function(){
     console.log(`안녕하세요 ${this.name} 입니다.`)
   }
 }
 
-var 학생1 = new Student('Kim', 20);
+// new Object()
+var 학생1 = new Student('Kim', 20); // name과 age 매개변수로 값 전달
 var 학생2 = new Student('Park', 21);
 var 학생3 = new Student('Lee', 30);
 
@@ -21,29 +25,47 @@ var 학생3 = new Student('Lee', 30);
 
 // 문제 2
 // 다음 코드의 출력값은?
-function Parent(){
-  this.name = 'Kim';
-}
+// function Parent(){
+//   this.name = 'Kim';
+// }
 
-var a = new Parent();
+// var a = new Parent();
 
-// 부모 prototype에 {name : 'Park'}을 추가하라는 뜻
-a.__proto__.name = 'Park';
-console.log(a.name)
+// // 부모 prototype에 {name : 'Park'}을 추가하라는 뜻
+// a.__proto__.name = 'Park';
+// console.log(a.name)
 
 
 // 문제 3
 // 문제1 에서 생성한 이름 출력해주는 함수에 prototype을 추가 했는데 의도대로 동작하지 않는 이유는?
-function Student1(이름, 나이){
-  this.name = 이름;
-  this.age = 나이;
-}
+// function Student1(이름, 나이){
+//   this.name = 이름;
+//   this.age = 나이;
+// }
 
-Student1.prototype.sayHi = () => {
-  // console.log(this)
-  console.log('안녕 나는 ' + this.name + '이야');
-}
+// Student1.prototype.sayHi = () => {
+//   // console.log(this)
+//   console.log('안녕 나는 ' + this.name + '이야');
+// }
 
-var 학생1 = new Student1('Kim', 20);
+// var 학생1 = new Student1('Kim', 20);
 
-학생1.sayHi();
+// 학생1.sayHi();
+
+
+// 문제 4
+// 모든 Array에 적용할 수 있는 함수 생성
+
+// Array.prototype.remove3 = function(){
+//   console.log(this)
+//   for(var i = 0; i < this.length; i++) {
+//     if(this[i] == 3) {
+//       this.splice(i, 1);
+//     }
+//   }
+// }
+
+// var arr = [1,2,3,4];
+
+// arr.remove3();
+// console.log(arr);
