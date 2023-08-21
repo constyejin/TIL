426532ccc1ba05a36c544cbb39b1ee2a08a95bb7
constyejin <lightup.jin@gmail.com>
2023-08-21 22:08:05 +0900

    ✨ Iterate through an object using for...in loop.

--- Javascript/Javascript Basic/20. select input/script.js
+++ Javascript/Javascript Basic/20. select input/script.js
@@ -14,7 +14,8 @@
 
 let inputs = document.querySelectorAll('.select-input');
 // 데이터 length 만큼 option 생성하기
-let pants = [25, 26, 27, 28, 29, 30, 31]
+let shirt = [90, 95, 100, 105];
+let pants = [25, 26, 27, 28, 29, 30, 31];
 
 inputs[0].addEventListener('input', function(){
   let formOne = this.value;
@@ -22,14 +23,16 @@ inputs[0].addEventListener('input', function(){
 
   if(formOne == '셔츠') {
     formTwo.classList.remove('form-hide');
-    formTwo.innerHTML = `
-      <option>90</option>
-      <option>95</option>
-    `
+    formTwo.innerHTML = '';
+    shirt.forEach(function(item){
+      let shirtOpt = `<option>${item}</option>`
+      formTwo.insertAdjacentHTML('beforeend', shirtOpt);
+    })
+
   // 바지 선택 했을 때 26, 27 option 나오게 작성 (JS로 HTML 추가)
   } else if(formOne == '바지') {
     formTwo.classList.remove('form-hide');
-    formTwo.innerHTML = ''
+    formTwo.innerHTML = '';
     // for(let i = 0; i < pants.length; i++) {
     //   let pantsOpt = `<option>${pants[i]}</option>`
     //   formTwo.insertAdjacentHTML('beforeend', pantsOpt);
@@ -66,4 +69,22 @@ document.querySelector('.box').insertAdjacentHTML('beforeend', txt);
 // 2. .insertAdjacentHTML()
 
 
+// object 자료 갯수만큼 반복문
+// for ~ in 사용
+let obj = {
+  name : 'yejin',
+  age : 20
+}
+// key 값(자료 이름)
+for(let key in obj) {
+  console.log(obj[key]);
+}
 
+// 반복문의 용도
+// 1. 코드 반복
+// 2. array, object 데이터 전부 꺼낼 때
+
+
+// arrow function 사용하면 함수 안의 this 값이 달라질 수 있다.
+// arrow function 안에서는 바깥에 있던 this값을 그대로 가져다 쓴다.
+// function 안에서 사용하면 this가 알맞게 재정의 된다.
