c90d1796d49fa21b26794b3c896251840b6e79ae
constyejin <lightup.jin@gmail.com>
2023-09-21 01:13:35 +0900

    ✨ Create and Complete Main component

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 // import Box from './component/Box';
 // import Map from './component/Map';
 import Header from './component/Header/Header';
+import Main from './component/Main/Main';
 
 function App() {
   return (
     <div className="App">
       {/* <Map/> */}
       <Header/>
+      <Main/>
     </div>
   );
 }
