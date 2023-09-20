b0b303a4d5030d4267814d9f9c46090d7d4aa5fd
constyejin <lightup.jin@gmail.com>
2023-09-20 21:25:15 +0900

    ✨ Commenting out 'react StrictMode'

--- React/react-basic/src/index.js
+++ React/react-basic/src/index.js
@@ -6,9 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // 리액트는 예상 못한 에러를 잡기위해 Strit 모드를 지원한다.
+  // 랜더링 단계에서 함수를 한 번 호출하고, 후에 한 번 더 호출해서 동일한 값이 나오는지 비교한다.
+  // React 개발 환경에서만 두 번 호출되고 배포 했을 때는 상관 X
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
