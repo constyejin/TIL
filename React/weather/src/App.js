e8402a3dffa1c45bcb858405b1020803da560e8f
constyejin <lightup.jin@gmail.com>
2023-09-25 23:18:06 +0900

    ✨ Intergrate Weather Open API

--- React/weather/src/App.js
+++ React/weather/src/App.js
@@ -1,9 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
+// 1. 앱이 실행되면 현재위치 기반의 날씨가 보인다.
+// 2. 날씨 정보에는 도시, 섭씨, 화씨 날씨 상태
+// 3. 5개의 버튼 (현재위치, 나머지 4개 다른 도시 위치)
+// 4. 도시버튼을 클릭하면 해당 도시 날씨를 보여준다.
+// 5. 데이터가 나오는 동안 로딩 스피너
+
+// 앱이 실행 되자마자 => 리액트 라이프 사이클 useEffect 사용
 function App() {
+  // 현재 내 위치 가져오기
+
+  const getCurrentLocation = () => {
+    // Geolocation API 검색
+    // navigator 라는 자바스크립트 기본 객체 사용
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon)
+    })
+  }
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0f26ad7c4499a0f6c1015b173b85237`;
+    let response = await fetch(url)
+    let data = await response.json();
+    console.log(data)
+  }
+
+  
+  useEffect(() => {
+    getCurrentLocation()
+  },[]);
+
   return (
-    <div className="App">
+    <div>
       
     </div>
   );
