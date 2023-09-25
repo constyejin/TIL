deb2912e72fb3441f12050a9d77369a4b776f018
constyejin <lightup.jin@gmail.com>
2023-09-25 23:25:33 +0900

    ✨ Create Weather component

---
+++ React/weather/src/component/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Weather = () => {
+  return (
+    <div>
+      <div>대전</div>
+      <h2>30도 / 230</h2>
+      <h3>맑은 하늘</h3>
+    </div>
+
+  )
+}
+
+export default Weather;
