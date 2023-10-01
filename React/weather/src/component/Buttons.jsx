ca7e499f25ae9cce94533f3f00c57689055df300
constyejin <lightup.jin@gmail.com>
2023-10-01 18:08:08 +0900

    ♻️ Invoke the state change function for city when button is clicked

--- React/weather/src/component/Buttons.jsx
+++ React/weather/src/component/Buttons.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 
-export const Buttons = ({cities}) => {
+export const Buttons = ({cities, setCity}) => {
   return (
     <div className='btn-list'>
       {
         cities.map((item, i) => {
-          return <Button key={i} variant="primary">{item}</Button>
+          return <Button key={i} onClick={() => setCity(item)} variant="primary">{item}</Button>
         })
       }
       {/* <Button onClick={props.currentLocation} variant="primary">Current Location</Button>{' '}
