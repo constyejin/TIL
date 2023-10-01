e85d78d3f657d17018f6076e3ebc870a2620516b
constyejin <lightup.jin@gmail.com>
2023-10-01 13:35:52 +0900

    ♻️ Changed function name

--- React/weather/src/App.js
+++ React/weather/src/App.js
@@ -21,15 +21,15 @@ function App() {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      getWeatherByCurrentLocation(lat, lon)
+      getWeather(lat, lon)
     })
   }
 
-  const getWeatherByCurrentLocation = async (lat, lon) => {
+  const getWeather = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0f26ad7c4499a0f6c1015b173b85237&units=metric`;
     let response = await fetch(url)
     let data = await response.json();
-    console.log(data)
+    // console.log(data)
     setWeather(data)
   }
 
