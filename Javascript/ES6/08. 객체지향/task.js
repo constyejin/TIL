76fba112c64e7b5f57eb6ba73c789b473530f2ba
constyejin <lightup.jin@gmail.com>
2023-11-15 20:53:30 +0900

    ✨ Create 3 objects using constrctor syntax

--- Javascript/ES6/08. 객체지향/task.js
+++ Javascript/ES6/08. 객체지향/task.js
@@ -1,6 +1,20 @@
 // 문제 1
 // constructor 문법을 사용해서 똑같은 object 3개 한번에 뽑기
 // 학생1.sayHi()로 사용하면  "안녕 나는 Kim이야" 라는 글자가 나오도록 sayHi()함수 constructor 안에 추가
-var 학생1 = { name : 'Kim', age : 20 }
-var 학생2 = { name : 'Park', age : 21 }
-var 학생3 = { name : 'Lee', age : 22 }
+
+function Student(name, age){
+  this.name = name;
+  this.age = age;
+  this.sayHi = function(){
+    console.log(`안녕하세요 ${this.name} 입니다.`)
+  }
+}
+
+var 학생1 = new Student('Kim', 20);
+var 학생2 = new Student('Park', 21);
+var 학생3 = new Student('Lee', 30);
+
+학생1.sayHi();
+학생2.sayHi();
+학생3.sayHi();
+
