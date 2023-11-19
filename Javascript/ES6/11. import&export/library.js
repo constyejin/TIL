a853ec99380bc02a44eb9b253a2a1001aabfe66a
constyejin <lightup.jin@gmail.com>
2023-11-19 15:59:34 +0900

    Modular Development using import / export

---
+++ Javascript/ES6/11. import&export/library.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var c = 30;
+
+// export default 문법
+// export default 내보낼거
+// export default는 파일당 1회 사용
+// export default a;
+
+// 여러개 내보내는 export 문법
+// exprot {내보낼것들}
+export {a,b};
+// export와 export default 동시 사용 가능
+export default c;
