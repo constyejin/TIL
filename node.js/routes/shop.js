037a6cb6a2e9b2ec9238a86e0e3d5502a11db4bc
constyejin <lightup.jin@gmail.com>
2023-12-10 22:49:10 +0900

    ✨ DB connection from router file

--- node.js/routes/shop.js
+++ node.js/routes/shop.js
@@ -1,5 +1,15 @@
 const router = require('express').Router()
 
+let connectData = require('./../database');
+
+let db;
+connectData.then((client)=>{
+  console.log('DB연결성공');
+  db = client.db('forum');
+}).catch((err)=>{
+  console.log(err);
+})
+
 // 공통된 URL 축약
 router.get('/shirts', (request, response) => {
   response.send('셔츠 파는 페이지')
