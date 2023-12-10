803a89d7c3dcd61941edbd2b836ce7de8d97cd8b
constyejin <lightup.jin@gmail.com>
2023-12-10 23:12:55 +0900

    checkLogin function exports

---
+++ node.js/checkLogin.js
@@ -0,0 +1,9 @@
+let checkLogin = function checkLogin (request, response, next) {
+  // 미들웨어 함수에선 요청, 응답 자유롭게 사용 가능하다.
+  if(!request.user) {
+    response.send('로그인 하세요.');
+  }
+  next()
+}
+
+module.exports = checkLogin
