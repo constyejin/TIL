5a8215ed0513bc728f31e8b4152d9d69dd80a3e9
constyejin <lightup.jin@gmail.com>
2023-12-11 00:33:19 +0900

    🐛 Array of returned documents

--- node.js/routes/search.js
+++ node.js/routes/search.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
+let connectData = require('./../database');
+
+let db;
+connectData.then((client)=>{
+  console.log('DB연결성공');
+  db = client.db('forum');
+}).catch((err)=>{
+  console.log(err);
+})
 
 router.get('/search', async(request, response) => {
-  console.log(request.query.val)
+  // console.log(request.query.val)
+  let result = await db.collection('post').find({ title : request.query.val }).toArray();
+  response.render('search.ejs', { posts : result })
 })
 
 module.exports = router
