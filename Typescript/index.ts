dfb8952569cd0a291cb23f87bd95c5aed17f6f95
constyejin <lightup.jin@gmail.com>
2024-01-04 23:47:56 +0900

    ✨ Specify object type

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -73,3 +73,15 @@ let age :number | undefined = undefined;
 let married :boolean = false;
 let 철수 :(string|number|undefined|boolean)[] = [user, age, married];
 
+let 학교 :{
+  score : (number | boolean)[],
+  teacher : string,
+  friend : string | string[]
+} = {
+  score : [100, 97, 84],
+  teacher : 'Phil',
+  friend : 'John'
+}
+
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher];
