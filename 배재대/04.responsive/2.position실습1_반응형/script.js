9ce8c3d50979b05ae6dfdab776f000ce0e851c0c
constyejin <lightup.jin@gmail.com>
2024-05-29 17:28:43 +0900

    ✨ Sidebar open / close

--- 배재대/04.responsive/2.position실습1_반응형/script.js
+++ 배재대/04.responsive/2.position실습1_반응형/script.js
@@ -9,3 +9,17 @@ moreBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
     popup.classList.remove('active');
 })
+
+
+let sidebar = document.querySelector('.sidebar');
+let sidebarBtn = document.querySelector('.sidebar-btn');
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
+
+
+sidebarBtn.addEventListener('click', function() {
+  sidebar.classList.add('active');
+})
+
+sidebarCloseBtn.addEventListener('click', function() {
+  sidebar.classList.remove('active');
+})
