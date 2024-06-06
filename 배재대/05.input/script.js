da53af2dfd682df891b820c530a37c076102d600
constyejin <lightup.jin@gmail.com>
2024-06-06 16:40:48 +0900

    ✨ Separation scrips files

---
+++ 배재대/05.input/script.js
@@ -0,0 +1,56 @@
+let submitBtn = document.getElementById('submit-btn');
+let joinForm = document.getElementById('join-form');
+let idInput = document.getElementById('id');
+let idAlert = document.querySelector('.id-alert');
+
+submitBtn.addEventListener('click', function(e) {
+  // keyup : 키를 떼었을 때 이벤트가 발생 | 자바스크립트 이벤트 종류
+  if(idInput.value.length <= 8) {
+    e.preventDefault();
+    // alert('아이디를 8글자 이상 입력하세요.');
+    idAlert.classList.add('active');
+  } else {
+    joinForm.submit();
+  }
+})
+
+// 한국 표준시 (Korea Standard Time)
+let kst = new Date();
+let getYear = kst.getFullYear(); // 년
+let getMonth = kst.getMonth(); // 월
+let getDate = kst.getDate(); // 일
+
+let year = document.getElementById('year');
+let month = document.getElementById('month');
+let date = document.getElementById('date');
+
+for(let i = 1900; i <= getYear; i++){
+  // let option = document.createElement('option');
+  // option.innerHTML = i;
+  // document.getElementById('year').appendChild(option);
+
+  let options = `<option>${i}</option>`;
+  // beforebegin, afterbegin, beforeend, afterend
+  // 추가하고싶은곳.insertAdjacentHTML('위치', 추가할요소);
+  year.insertAdjacentHTML('beforeend', options);
+
+  // $("#year").append("<option>"+i+"</option>");
+}
+
+for(let i = 1; i <= 12; i++){
+  let option = document.createElement('option');
+  option.innerHTML = i;
+  month.appendChild(option);
+
+  // $("#month").append("<option>"+i+"</option>");
+}
+
+for(let i = 1; i <= 31; i++){
+  let option = document.createElement('option');
+  option.innerHTML = i;
+  date.appendChild(option);
+
+  // $("#date").append("<option>"+i+"</option>");
+}
+
+console.log()
