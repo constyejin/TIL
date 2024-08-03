05f96ab33920eddbea8e61e9b4ae9e074d113788
constyejin <lightup.jin@gmail.com>
2024-08-03 19:14:29 +0900

    🎨 Change Directory name to Lv_0

--- Algorithm/Programmers/Lv.0/두 수의 나눗셈/script.js
+++
@@ -1,7 +0,0 @@
-const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
-
-console.log(solution(3, 2)) // 1500
-console.log(solution(7, 3)) // 2333
-console.log(solution(1, 16)) // 62
-
-
