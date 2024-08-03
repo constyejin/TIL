2c384c1dc29488721868820925316ea0cf9fba76
constyejin <lightup.jin@gmail.com>
2024-08-03 19:16:16 +0900

    📝 Math.floor() & Math.trunc()

--- Algorithm/Programmers/Lv_0/두수의나눗셈/script.js
+++ Algorithm/Programmers/Lv_0/두수의나눗셈/script.js
@@ -1,7 +1,7 @@
-const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
+const solution = (num1, num2) => Math.trunc((num1 / num2) * 1000);
 
 console.log(solution(3, 2)) // 1500
 console.log(solution(7, 3)) // 2333
 console.log(solution(1, 16)) // 62
 
-
+// Math.floor()는 소수점을 내림하므로 음수인 경우를 생각해서 소수점 이하를 없애고 싶을 때는 Math.trunc()를 사용하는 게 좋다.
