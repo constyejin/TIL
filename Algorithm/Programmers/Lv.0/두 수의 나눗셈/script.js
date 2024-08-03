d1074ca4ae1068eef941a639966afa76691e6049
constyejin <lightup.jin@gmail.com>
2024-08-03 19:02:18 +0900

    ✨ Lv.0 두 수의 나눗셈

---
+++ Algorithm/Programmers/Lv.0/두 수의 나눗셈/script.js
@@ -0,0 +1,7 @@
+const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
+
+console.log(solution(3, 2)) // 1500
+console.log(solution(7, 3)) // 2333
+console.log(solution(1, 16)) // 62
+
+
