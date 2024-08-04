1109625ded6acab3439de225084037a8a7cdfd55
constyejin <lightup.jin@gmail.com>
2024-08-05 00:02:38 +0900

    ✨ 문제 풀이 .reduce( ) And for문

--- Algorithm/Programmers/Lv_0/배열의평균값/script.js
+++ Algorithm/Programmers/Lv_0/배열의평균값/script.js
@@ -1,15 +1,37 @@
 // 내 코드
 const arr = [12, 5, 7, 32, 10];
 
-// function solution(numbers) {
-//   let answer = numbers.reduce((a, b) => a + b) / numbers.length;
-//   return answer;
-// }
+function solution(numbers) {
+  let answer = numbers.reduce((a, b) => a + b) / numbers.length;
+  return answer;
+}
+console.log(solution(arr))
 
-// console.log(solution(arr))
 
- 
+// *** .reduce() 
+
 // const solution = (numners) => numners.reduce((acc, cur) => acc + cur) / numners.length;
 // console.log(solution(arr))
 
 
+// const solution = (numbers) =>
+//   numbers.reduce((acc, cur) => {
+//     return acc + cur;
+//   }, 0) / numbers.length
+
+//   console.log(solution(arr))
+  
+  
+// *** for문
+// function solution(numbers) {
+//   let answer = 0;
+
+//   for(i of numbers) {
+//     answer += i;
+//   }
+//   answer = answer / numbers.length;
+
+//   return answer;
+// }
+
+// console.log(solution(arr))
