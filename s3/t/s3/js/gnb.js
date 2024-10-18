f29b0dd6d29b233531a14ec5cc3b5bc7c7b7cada
constyejin <lightup.jin@gmail.com>
2024-10-18 23:52:55 +0800

    🔥Delete s3 project

--- s3/t/s3/js/gnb.js
+++
@@ -1,9 +0,0 @@
-const gnbItem = document.querySelectorAll('.gnb li');
-
-gnbItem.forEach(item => {
-  item.addEventListener('click', () => {
-    gnbItem.forEach(item => item.classList.remove('is-active'));
-    item.classList.add('is-active');
-  })
-})
-
