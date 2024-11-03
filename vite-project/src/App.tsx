a81981812d8c30a4e00288a378b2cf7b88012b5f
constyejin <lightup.jin@gmail.com>
2024-11-03 10:00:00 +0800

    💄 createGlobalStyle

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,32 @@
-import './App.css';
+// Styled Components
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+
+// Components
 import Hello from './components/Hello';
 import Counter from './components/Counter';
 import TodoList from './components/TodoList';
 
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial, sans-serif;
+    color: #fff;
+    background-color: #333;
+  }
+
+  * {
+    font-family: Arial, sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <TodoList />
     </>
   )
